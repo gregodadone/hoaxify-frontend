bccbd5dbc3bb0610fe8fd7f8a15e7b6ceea437c1
Grego Dadone <grego.dadone@gmail.com>
2020-06-08 21:57:45 -0300

    Completed layout

--- src/pages/user-sign-up-page.spec.js
+++ src/pages/user-sign-up-page.spec.js
@@ -1,15 +1,53 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { UserSignUpPage } from './user-sign-up-page';
+
+beforeEach(cleanup);
 
 describe('User Sign Up Page', () => {
     describe('Layout', () => {
         it('has header of Sign Up', () => {
             const { container } = render(<UserSignUpPage />);
             const header = container.querySelector('h1');
-
             expect(header).toHaveTextContent('Sign Up');
+        });
+
+        it('has input for display name', () => {
+            const { queryByPlaceholderText } = render(<UserSignUpPage />);
+            const displayNameInput = queryByPlaceholderText('Your display name');
+            expect(displayNameInput).toBeInTheDocument();
+        });
+
+        it('has input for username', () => {
+            const { queryByPlaceholderText } = render(<UserSignUpPage />);
+            const userNameInput = queryByPlaceholderText('Your username');
+            expect(userNameInput).toBeInTheDocument();
+        });
+
+        it('has input for password', () => {
+            const { queryByPlaceholderText } = render(<UserSignUpPage />);
+            const passwordInput = queryByPlaceholderText('Your password');
+            expect(passwordInput).toBeInTheDocument();
+        });
+
+        it('has input for repeat password', () => {
+            const { queryByPlaceholderText } = render(<UserSignUpPage />);
+            const repeatPasswordInput = queryByPlaceholderText('Repeat your password');
+            expect(repeatPasswordInput).toBeInTheDocument();
+        });
+
+        it('has password type for password input', () => {
+            const { queryByPlaceholderText } = render(<UserSignUpPage />);
+            const passwordInput = queryByPlaceholderText('Your password');
+            expect(passwordInput.type).toBe('password');
+        });
+
+        it('has submit button', () => {
+            const { container } = render(<UserSignUpPage />);
+            const submitButton = container.querySelector('button');
+            expect(submitButton).toBeInTheDocument();
         })
     })
+
 })
